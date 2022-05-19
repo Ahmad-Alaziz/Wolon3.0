41134ff19c59ea69fddafad3e1d8b882db85770c
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-05-19 23:11:43 +0200

    waku implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/ethereum-provider";
 import { Contract, providers, utils } from "ethers";
+import { Waku } from "js-waku";
 
-import { formatAuthMessage } from "./utils";
+import {
+  formatAuthMessage,
+  sendMessage,
+  SimpleChatMessage,
+  ContentTopic,
+} from "./utils";
 import { DAI } from "./constants";
 
 const App = () => {
+  const [waku, setWaku] = useState(undefined);
+  const [wakuStatus, setWakuStatus] = useState("None");
+  const [chainId, setChainId] = useState(1);
+  const [address, setAddress] = useState("");
+  const [provider, setProvider] = useState();
+  const [sendCounter, setSendCounter] = useState(0);
+  const [messages, setMessages] = useState([]);
+
   const web3Modal = new Web3Modal({
     network: "mainnet",
     cacheProvider: true,
@@ -19,10 +33,6 @@ const App = () => {
       },
     },
   });
-
-  const [chainId, setChainId] = useState(1);
-  const [address, setAddress] = useState("");
-  const [provider, setProvider] = useState();
 
   function reset() {
     console.log("reset");
@@ -66,6 +76,55 @@ const App = () => {
     console.log("res", res);
   }
 
+  const sendMessageOnClick = () => {
+    if (wakuStatus !== "Ready") return;
+
+    sendMessage(`Here is message #${sendCounter}`, waku, new Date()).then(() =>
+      console.log("Message sent")
+    );
+    setSendCounter(sendCounter + 1);
+  };
+
+  const processIncomingMessage = useCallback((wakuMessage) => {
+    if (!wakuMessage.payload) return;
+
+    const { text, timestamp } = SimpleChatMessage.decode(wakuMessage.payload);
+
+    const time = new Date();
+    time.setTime(timestamp);
+
+    const message = { text, timestamp: time };
+
+    setMessages((messages) => {
+      return [message].concat(messages);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!!waku) return;
+    if (wakuStatus !== "None") return;
+
+    setWakuStatus("Starting");
+
+    Waku.create({ bootstrap: { default: true } }).then((waku) => {
+      setWaku(waku);
+      setWakuStatus("Connecting");
+      waku.waitForRemotePeer().then(() => {
+        setWakuStatus("Ready");
+      });
+    });
+  }, [waku, wakuStatus]);
+
+  useEffect(() => {
+    if (!waku) return;
+
+    waku.relay.addObserver(processIncomingMessage, [ContentTopic]);
+
+    return function cleanUp() {
+      waku.relay.deleteObserver(processIncomingMessage, [ContentTopic]);
+    };
+  }, [waku, wakuStatus, processIncomingMessage]);
+
   return (
     <div>
       <div>{provider ? "Connected!" : "Not connected"}</div>
@@ -78,6 +137,28 @@ const App = () => {
       ) : (
         <button onClick={connect}>Connect</button>
       )}
+      <div>
+        <header>
+          <p>Waku node's status: {wakuStatus}</p>
+          <button
+            onClick={sendMessageOnClick}
+            disabled={wakuStatus !== "Ready"}
+          >
+            Send Message
+          </button>
+          <ul>
+            {messages.map((msg) => {
+              return (
+                <li>
+                  <p>
+                    {msg.timestamp.toString()}: {msg.text}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </header>
+      </div>
     </div>
   );
 };
