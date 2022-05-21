04da4ccca9c78ac0f7240bc257d55e73349ce027
Pawel Kowalewski <pkowalewski@microstrategy.com>
2022-05-22 00:12:24 +0200

    contract implementation added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { ethers } from "ethers";
 
-import Navbar from '../components/Navbar';
-import Sidebar from '../components/Sidebar';
-import Footer from '../components/Footer';
-import HeroSection from '../components/HeroSection';
-import InfoSection from '../components/InfoSection';
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
 
 import {
   sectionOne,
@@ -12,26 +13,31 @@ import {
   sectionThree,
   sectionFour,
   sectionFive,
-} from '../components/InfoSection/Data';
-import Services from '../components/Services';
+} from "../components/InfoSection/Data";
+import Services from "../components/Services";
 
-import Web3Modal from 'web3modal';
-import WalletConnectProvider from '@walletconnect/ethereum-provider';
-import { Contract, providers, utils } from 'ethers';
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/ethereum-provider";
+import { Contract, providers, utils } from "ethers";
 
-import { formatAuthMessage } from '../utils';
-import { DAI } from '../constants';
-import StepExampleGroup from '../components/Steps';
+import { formatAuthMessage, transformMemberData } from "../utils";
+import { DAI } from "../constants";
+import Wolon from "../abi/Wolon.json";
+import StepExampleGroup from "../components/Steps";
+
+const { REACT_APP_CONTRACT_ADDRESS } = process.env;
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
+  const [dappContract, setDappContract] = useState(null);
+  const [memberNFT, setMemberNFT] = useState(null);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
 
   const web3Modal = new Web3Modal({
-    network: 'mainnet',
+    network: "mainnet",
     cacheProvider: true,
     providerOptions: {
       walletconnect: {
@@ -44,23 +50,23 @@ function Home() {
   });
 
   const [chainId, setChainId] = useState(1);
-  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState("");
   const [provider, setProvider] = useState();
 
   function reset() {
-    console.log('reset');
-    setAddress('');
+    console.log("reset");
+    setAddress("");
     setProvider(undefined);
     web3Modal.clearCachedProvider();
   }
 
   async function connect() {
     if (!process.env.REACT_APP_INFURA_ID) {
-      throw new Error('Missing Infura Id');
+      throw new Error("Missing Infura Id");
     }
     const web3Provider = await web3Modal.connect();
 
-    web3Provider.on('disconnect', reset);
+    web3Provider.on("disconnect", reset);
 
     const accounts = await web3Provider.enable();
     setAddress(accounts[0]);
@@ -70,24 +76,59 @@ function Home() {
     setProvider(provider);
   }
 
+  const mintNFT = async () => {
+    try {
+      const mintTxn = await dappContract.mintMembershipNFT();
+      await mintTxn.wait();
+    } catch (error) {
+      console.warn("Error: ", error);
+    }
+  };
+
   async function signMessage() {
     if (!provider) {
-      throw new Error('Provider not connected');
+      throw new Error("Provider not connected");
     }
     const msg = formatAuthMessage(address, chainId);
-    const sig = await provider.send('personal_sign', [msg, address]);
-    console.log('Signature', sig);
-    console.log('isValid', utils.verifyMessage(msg, sig) === address);
+    const sig = await provider.send("personal_sign", [msg, address]);
+    console.log("Signature", sig);
+    console.log("isValid", utils.verifyMessage(msg, sig) === address);
   }
 
   async function transferDai() {
     if (!provider) {
-      throw new Error('Provider not connected');
+      throw new Error("Provider not connected");
     }
     const contract = new Contract(DAI.address, DAI.abi, provider.getSigner());
-    const res = await contract.transfer(address, utils.parseEther('1'));
-    console.log('res', res);
+    const res = await contract.transfer(address, utils.parseEther("1"));
+    console.log("res", res);
   }
+
+  useEffect(() => {
+    if (!provider) {
+      return;
+    }
+    const signer = provider.getSigner();
+    const dappContract = new ethers.Contract(
+      REACT_APP_CONTRACT_ADDRESS,
+      Wolon.abi,
+      signer
+    );
+    setDappContract(dappContract);
+
+    const checkIsMember = async () => {
+      try {
+        const memberNFT = await dappContract.checkIfUserHasNFT();
+        if (memberNFT) {
+          setMemberNFT(transformMemberData(memberNFT));
+        }
+      } catch (error) {
+        console.warn("Error: ", error);
+      }
+    };
+
+    checkIsMember();
+  }, [address, provider]);
 
   return (
     <>
@@ -99,7 +140,12 @@ function Home() {
       <InfoSection {...sectionThree} />
       <InfoSection {...sectionFour} />
       <Services />
-      <StepExampleGroup {...sectionFive} connect={connect} address={address} />
+      <StepExampleGroup
+        {...sectionFive}
+        connect={connect}
+        address={address}
+        mintNFT={mintNFT}
+      />
       <Footer />
     </>
   );
