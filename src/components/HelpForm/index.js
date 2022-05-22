51b189421c2fbf50f1dbe87b246677a9d6c545e4
aalaziz <aalaziz@microstrategy.com>
2022-05-22 14:47:35 +0200

    Add Alert

--- src/components/HelpForm/index.js
+++ src/components/HelpForm/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Web3Storage } from 'web3.storage';
 import 'react-toggle/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
 
 import {
   Container,
@@ -16,6 +18,7 @@ import {
 } from './HelpFormElements';
 
 import Toggle from 'react-toggle';
+import ActivityIndicator from '../ActivityIndicator';
 
 const options = [
   { value: 'chocolate', label: 'Chocolate' },
@@ -63,8 +66,13 @@ const HelpForm = ({ dappContract }) => {
   const [description, setDescription] = useState(false);
   const [category, setCategory] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    setIsLoading(true);
 
     try {
       const storage = new Web3Storage({
@@ -101,12 +109,51 @@ const HelpForm = ({ dappContract }) => {
     } catch (error) {
       console.warn('Error: ', error);
     }
+    setIsLoading(false);
   };
 
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
   return (
     <Container>
       <FormWrap>
         <FormContent>
+          <div
+            style={{
+              width: '300px',
+              alignSelf: 'center',
+              position: 'absolute',
+              zIndex: 10000,
+              left: 'auto',
+              top: 'auto',
+            }}
+          >
+            {isValid ? (
+              <Alert variant='success' dismissible>
+                <Alert.Heading>Success!</Alert.Heading>
+                <p>Your help request has been published successfully!</p>
+                <hr />
+                <p className='mb-0'>
+                  You have opened a continious cashflow that will be sending
+                  money as long as your Advert is being shown, you can delete
+                  the Advert and cancel the subscription at any point and you
+                  won't have to pay for any second more than you need!
+                </p>
+              </Alert>
+            ) : (
+              <Alert variant='danger'>
+                <Alert.Heading>Oh Snap!</Alert.Heading>
+                <p>It seems like something went wrong :/</p>
+                <hr />
+                <p className='mb-0'>
+                  If you already have a running Advert you need to cancel it
+                  first. You can only have one Advert running at a time.
+                </p>
+              </Alert>
+            )}
+          </div>
+
           <Form onSubmit={handleSubmit}>
             <FormH1>Create a Help Request</FormH1>
             <FormLabel htmlFor='for'>Category</FormLabel>
